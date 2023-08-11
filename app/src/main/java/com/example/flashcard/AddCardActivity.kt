e3b1591dd00cd322b9dd53079bd7da74e575ca26
Debbie <dimanchemydl133@gmail.com>
2023-08-11 15:32:48 -0400

    lab2- optional features

--- app/src/main/java/com/example/flashcard/AddCardActivity.kt
+++ app/src/main/java/com/example/flashcard/AddCardActivity.kt
@@ -1,11 +1,65 @@
 package com.example.flashcard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
 
 class AddCardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_card)
+
+
+
+        val ivcancel = findViewById<ImageView>(R.id.ivCancel)
+        val ivsave = findViewById<ImageView>(R.id.ivSave)
+
+        val etquestion = findViewById<EditText>(R.id.etQuestion)
+        val etanswer0 = findViewById<EditText>(R.id.etAnswer0)
+        val etanswer1 = findViewById<EditText>(R.id.etAnswer1)
+        val etanswer2 = findViewById<EditText>(R.id.etAnswer2)
+
+
+        etquestion.setText(intent.getStringExtra("question_edit"))
+        etanswer0.setText(intent.getStringExtra("answer_edit0"))
+        etanswer1.setText(intent.getStringExtra("answer_edit1"))
+        etanswer2.setText(intent.getStringExtra("answer_edit2"))
+
+
+
+        ivsave.setOnClickListener(){
+            val data = Intent()
+
+            val etquestion = etquestion.text.toString()
+            val etanswer0 = etanswer0.text.toString()
+            val etanswer1 = etanswer1.text.toString()
+            val etanswer2 = etanswer2.text.toString()
+
+
+            if(etquestion.isEmpty() or etanswer0.isEmpty() or etanswer1.isEmpty() or etanswer2.isEmpty()){
+                Toast.makeText(applicationContext, R.string.toast_message, Toast.LENGTH_SHORT).show()
+            }
+            else{
+
+                data.putExtra("question", etquestion)
+                data.putExtra("answer0", etanswer0)
+                data.putExtra("answer1", etanswer1)
+                data.putExtra("answer2", etanswer2)
+
+                setResult(RESULT_OK, data)
+
+                finish()
+            }
+        }
+
+        ivcancel.setOnClickListener(){
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/flashcard/MainActivity.kt
+++ app/src/main/java/com/example/flashcard/MainActivity.kt
@@ -1,22 +1,87 @@
 package com.example.flashcard
 
+import android.app.Activity
+import android.app.ProgressDialog.show
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.isVisible
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tvQuestion = findViewById<TextView>(R.id.tvQuestion)
-        val tvAnswer = findViewById<TextView>(R.id.tvAnswer)
 
-        tvQuestion.setOnClickListener{
-            tvQuestion.visibility= View.INVISIBLE
-            tvAnswer.visibility = View.VISIBLE
+        val tvQuestion = findViewById<TextView>(R.id.tvQuestion)
+        val tvAnswer0 = findViewById<TextView>(R.id.tvAnswer0)
+        val tvAnswer1 = findViewById<TextView>(R.id.tvAnswer1)
+        val tvAnswer2 = findViewById<TextView>(R.id.tvAnswer2)
+
+
+        val ivEdit = findViewById<ImageView>(R.id.ivEdit)
+        val ivAdd = findViewById<ImageView>(R.id.ivAdd)
+
+
+
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if(result.resultCode == Activity.RESULT_OK) {
+                val data = result.data
+
+                if (data != null) {
+                    val question = data.getStringExtra("question")
+                    val answer0 = data.getStringExtra("answer0")
+                    val answer1 = data.getStringExtra("answer1")
+                    val answer2 = data.getStringExtra("answer2")
+
+
+                    tvQuestion.text = question
+                    tvAnswer0.text = answer0
+                    tvAnswer1.text = answer1
+                    tvAnswer2.text = answer2
+
+
+                    val parentView = findViewById<View>(android.R.id.content)
+                    Snackbar.make(parentView, R.string.snackbar_message, Snackbar.LENGTH_LONG)
+                        .show()
+
+                } else {
+                }
+            }
         }
+
+        ivEdit.setOnClickListener() {
+
+            val intent = Intent(this, AddCardActivity::class.java)
+
+            val question_text = tvQuestion.text.toString()
+            val answer_text0 = tvAnswer0.text.toString()
+            val answer_text1 = tvAnswer1.text.toString()
+            val answer_text2 = tvAnswer2.text.toString()
+
+
+            intent.putExtra("question_edit", question_text);
+            intent.putExtra("answer_edit0", answer_text0);
+            intent.putExtra("answer_edit1", answer_text1);
+            intent.putExtra("answer_edit2", answer_text2);
+
+            resultLauncher.launch(intent)
+        }
+
+
+        ivAdd.setOnClickListener() {
+            val intent = Intent(this, AddCardActivity::class.java)
+            resultLauncher.launch(intent)
+        }
+
     }
 }
