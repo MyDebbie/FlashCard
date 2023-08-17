d20dd1e908400b7e4cb7cdb508f98ca7bfc8b10e
Debbie <dimanchemydl133@gmail.com>
2023-08-17 13:09:59 -0400

    save a card

--- app/src/main/java/com/example/flashcard/AddCardActivity.kt
+++ app/src/main/java/com/example/flashcard/AddCardActivity.kt
@@ -20,15 +20,11 @@ class AddCardActivity : AppCompatActivity() {
         val ivsave = findViewById<ImageView>(R.id.ivSave)
 
         val etquestion = findViewById<EditText>(R.id.etQuestion)
-        val etanswer0 = findViewById<EditText>(R.id.etAnswer0)
         val etanswer1 = findViewById<EditText>(R.id.etAnswer1)
-        val etanswer2 = findViewById<EditText>(R.id.etAnswer2)
 
 
         etquestion.setText(intent.getStringExtra("question_edit"))
-        etanswer0.setText(intent.getStringExtra("answer_edit0"))
         etanswer1.setText(intent.getStringExtra("answer_edit1"))
-        etanswer2.setText(intent.getStringExtra("answer_edit2"))
 
 
 
@@ -36,20 +32,16 @@ class AddCardActivity : AppCompatActivity() {
             val data = Intent()
 
             val etquestion = etquestion.text.toString()
-            val etanswer0 = etanswer0.text.toString()
             val etanswer1 = etanswer1.text.toString()
-            val etanswer2 = etanswer2.text.toString()
 
 
-            if(etquestion.isEmpty() or etanswer0.isEmpty() or etanswer1.isEmpty() or etanswer2.isEmpty()){
+            if(etquestion.isEmpty() or etanswer1.isEmpty()){
                 Toast.makeText(applicationContext, R.string.toast_message, Toast.LENGTH_SHORT).show()
             }
             else{
 
                 data.putExtra("question", etquestion)
-                data.putExtra("answer0", etanswer0)
                 data.putExtra("answer1", etanswer1)
-                data.putExtra("answer2", etanswer2)
 
                 setResult(RESULT_OK, data)
 
--- app/src/main/java/com/example/flashcard/MainActivity.kt
+++ app/src/main/java/com/example/flashcard/MainActivity.kt
@@ -18,20 +18,66 @@ import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var flashcardDatabase: FlashcardDatabase
+    var allFlashcards = mutableListOf<Flashcard>()
+    var currentCardDisplayedIndex = 0
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        flashcardDatabase = FlashcardDatabase(this)
+        allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+
+
         val tvQuestion = findViewById<TextView>(R.id.tvQuestion)
-        val tvAnswer0 = findViewById<TextView>(R.id.tvAnswer0)
         val tvAnswer1 = findViewById<TextView>(R.id.tvAnswer1)
-        val tvAnswer2 = findViewById<TextView>(R.id.tvAnswer2)
 
 
-        val ivEdit = findViewById<ImageView>(R.id.ivEdit)
+        if (allFlashcards.size > 0) {
+            tvQuestion.text = allFlashcards[0].question
+            tvAnswer1.text = allFlashcards[0].answer
+        }
+
+        //val ivEdit = findViewById<ImageView>(R.id.ivEdit)
         val ivAdd = findViewById<ImageView>(R.id.ivAdd)
+        val ivNext = findViewById<ImageView>(R.id.ivNext)
+
+        tvQuestion.setOnClickListener {
+            tvAnswer1.visibility = VISIBLE
+            tvQuestion.visibility = INVISIBLE
+
+        }
+
+        ivNext.setOnClickListener {
+            if (allFlashcards.size == 0) {
+                // return here, so that the rest of the code in this onClickListener doesn't execute
+                return@setOnClickListener
+            }
+
+            currentCardDisplayedIndex++
+
+            if(currentCardDisplayedIndex >= allFlashcards.size) {
+                Snackbar.make(
+                    findViewById<TextView>(R.id.tvQuestion),
+                    "You've reached the end of the cards, going back to start.",
+                    Snackbar.LENGTH_SHORT)
+                    .show()
+                currentCardDisplayedIndex = 0
+            }
+            tvAnswer1.visibility = INVISIBLE
+            tvQuestion.visibility = VISIBLE
+
+            allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+            val (question, answer1) = allFlashcards[currentCardDisplayedIndex]
+
+            tvQuestion.text = question
+            tvAnswer1.text = answer1
 
 
+        }
 
         val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if(result.resultCode == Activity.RESULT_OK) {
@@ -39,16 +85,27 @@ class MainActivity : AppCompatActivity() {
 
                 if (data != null) {
                     val question = data.getStringExtra("question")
-                    val answer0 = data.getStringExtra("answer0")
                     val answer1 = data.getStringExtra("answer1")
-                    val answer2 = data.getStringExtra("answer2")
+
+
+                    flashcardDatabase.insertCard(Flashcard(question.toString(), answer1.toString()))
+                    allFlashcards = flashcardDatabase.getAllCards().toMutableList()
 
 
                     tvQuestion.text = question
-                    tvAnswer0.text = answer0
                     tvAnswer1.text = answer1
-                    tvAnswer2.text = answer2
 
+                    // Save newly created flashcard to database
+                    if (question != null && answer1 != null) {
+                        flashcardDatabase.insertCard(Flashcard(question, answer1))
+                        // Update set of flashcards to include new card
+                        allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+                    } else {
+                        Log.e("TAG", "Missing question or answer to input into database. Question is $question and answer is $answer1")
+                    }
+                } else {
+                    Log.i("MainActivity", "Returned null data from AddCardActivity")
+                }
 
                     val parentView = findViewById<View>(android.R.id.content)
                     Snackbar.make(parentView, R.string.snackbar_message, Snackbar.LENGTH_LONG)
@@ -57,25 +114,21 @@ class MainActivity : AppCompatActivity() {
                 } else {
                 }
             }
-        }
-
-        ivEdit.setOnClickListener() {
-
-            val intent = Intent(this, AddCardActivity::class.java)
-
-            val question_text = tvQuestion.text.toString()
-            val answer_text0 = tvAnswer0.text.toString()
-            val answer_text1 = tvAnswer1.text.toString()
-            val answer_text2 = tvAnswer2.text.toString()
 
 
-            intent.putExtra("question_edit", question_text);
-            intent.putExtra("answer_edit0", answer_text0);
-            intent.putExtra("answer_edit1", answer_text1);
-            intent.putExtra("answer_edit2", answer_text2);
-
-            resultLauncher.launch(intent)
-        }
+//        ivEdit.setOnClickListener() {
+//
+//            val intent = Intent(this, AddCardActivity::class.java)
+//
+//            val question_text = tvQuestion.text.toString()
+//            val answer_text1 = tvAnswer1.text.toString()
+//
+//
+//            intent.putExtra("question_edit", question_text);
+//            intent.putExtra("answer_edit1", answer_text1);
+//
+//            resultLauncher.launch(intent)
+//        }
 
 
         ivAdd.setOnClickListener() {
